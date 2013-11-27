b0d5c675968db7c89749239b8bb38ee045e98340
Michele Ficarra <miche.ficarra@gmail.com>
2013-11-27 14:09:56 +0100

    Added an activity for prepare NFC tag

---
+++ src/com/google/developers/gdgfirenze/android/NfcWriteActivity.java
@@ -0,0 +1,14 @@
+package com.google.developers.gdgfirenze.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NfcWriteActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_nfc_write);
+	}
+
+}
